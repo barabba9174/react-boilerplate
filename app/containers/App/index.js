0683427cc81df8a4f7bf4a480ef4749e0c6f92ec
Barbara Schiavinato <barbara.schiavinato@rma-consulting.com>
2017-11-10 16:43:44 +0000

    test css module

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Table from 'containers/Table';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -36,6 +37,7 @@ export default function App() {
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
       <Header />
+      <Table />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
